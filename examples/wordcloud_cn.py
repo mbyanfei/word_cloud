63ffd9a3a35c7ab983276940e8bea8a470d56c2a
mbyanfei <feiyan08@gmail.com>
2022-10-29 23:35:13 +0000

    tried a few examples including cn

--- examples/wordcloud_cn.py
+++ examples/wordcloud_cn.py
@@ -11,9 +11,10 @@ most elegant the most popular Chinese word segmentation tool in python.
 You can use 'PIP install jieba'. To install it. As you can see,
 at the same time using wordcloud with jieba very convenient
 """
-
+#%%
 import jieba
 jieba.enable_parallel(4)
+#%%
 # Setting up parallel processes :4 ,but unable to run on Windows
 from os import path
 from imageio import imread
@@ -22,28 +23,30 @@ import os
 # jieba.load_userdict("txt\userdict.txt")
 # add userdict by load_userdict()
 from wordcloud import WordCloud, ImageColorGenerator
-
+#%%
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-
+#%%
 stopwords_path = d + '/wc_cn/stopwords_cn_en.txt'
 # Chinese fonts must be set
 font_path = d + '/fonts/SourceHanSerif/SourceHanSerifK-Light.otf'
-
+#%%
 # the path to save worldcloud
 imgname1 = d + '/wc_cn/LuXun.jpg'
 imgname2 = d + '/wc_cn/LuXun_colored.jpg'
+#%%
 # read the mask / color image taken from
 back_coloring = imread(path.join(d, d + '/wc_cn/LuXun_color.jpg'))
-
+#%%
 # Read the whole text.
 text = open(path.join(d, d + '/wc_cn/CalltoArms.txt')).read()
-
+#%%
+text
+#%%
 # if you want use wordCloud,you need it
 # add userdict by add_word()
 userdict_list = ['阿Ｑ', '孔乙己', '单四嫂子']
-
-
+#%%
 # The function for processing text with Jieba
 def jieba_processing_txt(text):
     for word in userdict_list:
@@ -61,29 +64,27 @@ def jieba_processing_txt(text):
         if not (myword.strip() in f_stop_seg_list) and len(myword.strip()) > 1:
             mywordlist.append(myword)
     return ' '.join(mywordlist)
-
-
+#%%
 wc = WordCloud(font_path=font_path, background_color="white", max_words=2000, mask=back_coloring,
                max_font_size=100, random_state=42, width=1000, height=860, margin=2,)
 
-
 wc.generate(jieba_processing_txt(text))
-
+#%%
 # create coloring from image
 image_colors_default = ImageColorGenerator(back_coloring)
-
+#%%
 plt.figure()
 # recolor wordcloud and show
 plt.imshow(wc, interpolation="bilinear")
 plt.axis("off")
 plt.show()
-
+#%%
 # save wordcloud
 wc.to_file(path.join(d, imgname1))
-
+#%%
 # create coloring from image
 image_colors_byImg = ImageColorGenerator(back_coloring)
-
+#%%
 # show
 # we could also give color_func=image_colors directly in the constructor
 plt.imshow(wc.recolor(color_func=image_colors_byImg), interpolation="bilinear")
@@ -92,6 +93,6 @@ plt.figure()
 plt.imshow(back_coloring, interpolation="bilinear")
 plt.axis("off")
 plt.show()
-
+#%%
 # save wordcloud
 wc.to_file(path.join(d, imgname2))
